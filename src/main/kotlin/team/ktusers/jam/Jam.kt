b44afd3fa70412d0aba78ab3a4a007338357d977
oglassdev <23040613+oglassdev@users.noreply.github.com>
2024-08-07 23:16:00 -0400

    update bladehunt libs

--- src/main/kotlin/team/ktusers/jam/Jam.kt
+++ src/main/kotlin/team/ktusers/jam/Jam.kt
@@ -1,10 +1,11 @@
 package team.ktusers.jam
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.coroutineScope
+import net.bladehunt.kotstom.coroutines.startSuspending
 import net.minestom.server.MinecraftServer
 
 suspend fun main() = coroutineScope {
     val server = MinecraftServer.init()
 
-    withContext(Dispatchers.IO) { server.start("0.0.0.0", 25565) }
+    server.startSuspending("0.0.0.0", 25565)
 }
