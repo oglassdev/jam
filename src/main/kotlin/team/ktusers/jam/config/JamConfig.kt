bdd5a230828e69c67e1ee4b866c5bb3f5d729f2a
oglassdev <23040613+oglassdev@users.noreply.github.com>
2024-08-09 12:18:42 -0400

    chore: concise config

--- src/main/kotlin/team/ktusers/jam/config/JamConfig.kt
+++ src/main/kotlin/team/ktusers/jam/config/JamConfig.kt
@@ -1,5 +1,24 @@
 package team.ktusers.jam.config
 
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import net.minestom.server.coordinate.Pos
 
-@Serializable data class JamConfig(val lobby: Lobby)
+@Serializable
+data class JamConfig(
+    val lobby: Lobby,
+    val game: Game
+) {
+    @Serializable
+    data class Lobby(
+        @SerialName("spawn_pos") val spawnPos: @Contextual Pos,
+        @SerialName("npc_pos") val npcPos: @Contextual Pos
+    )
+
+    @Serializable
+    data class Game(
+        @SerialName("spawn_pos") val spawnPos: @Contextual Pos,
+        @SerialName("spawn_radius") val spawnRadius: Double
+    )
+}
