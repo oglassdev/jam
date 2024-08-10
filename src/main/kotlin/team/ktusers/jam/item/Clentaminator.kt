4c21581ea9979e9543e5bdf85b98793d3da0dec7
oglassdev <23040613+oglassdev@users.noreply.github.com>
2024-08-09 21:45:45 -0400

    update clentaminator logic

--- src/main/kotlin/team/ktusers/jam/item/Clentaminator.kt
+++ src/main/kotlin/team/ktusers/jam/item/Clentaminator.kt
@@ -1,18 +1,20 @@
 package team.ktusers.jam.item
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import net.bladehunt.kotstom.GlobalEventHandler
 import net.bladehunt.kotstom.dsl.item.item
 import net.bladehunt.kotstom.dsl.item.itemName
 import net.bladehunt.kotstom.extension.adventure.text
+import net.bladehunt.kotstom.extension.asVec
+import net.bladehunt.kotstom.extension.roundToBlock
 import net.bladehunt.minigamelib.ext.game
 import net.kyori.adventure.text.format.NamedTextColor
 import net.minestom.server.event.player.PlayerUseItemEvent
 import net.minestom.server.item.Material
 import team.ktusers.jam.AdventureNbt
+import team.ktusers.jam.event.PlayerCleanseBlockEvent
+import team.ktusers.jam.event.PlayerPreCleanseBlockEvent
 import team.ktusers.jam.game.JamColor
 import team.ktusers.jam.game.JamGame
 
@@ -34,9 +36,10 @@ data class Clentaminator(
     override fun onUse(event: PlayerUseItemEvent) {
         val game = event.player.game as? JamGame? ?: return
 
-        CoroutineScope(Dispatchers.Default).launch {
-            game.cureChannel.send(Unit)
-        }
-        event.player.sendMessage("You used the clentaminator!!")
+        val point = event.player.getTargetBlockPosition(10)?.asVec()?.roundToBlock() ?: return
+        val block = event.instance.getBlock(point).takeIf { !it.isAir } ?: return
+        val preCleanse = PlayerPreCleanseBlockEvent(game, event.player, block, point)
+        GlobalEventHandler.call(preCleanse)
+        if (!preCleanse.isCancelled) GlobalEventHandler.call(PlayerCleanseBlockEvent(game, event.player, block, point))
     }
 }
