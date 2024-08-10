d12179acd5a22c7d187f9638c44e006763d81e14
oglassdev <23040613+oglassdev@users.noreply.github.com>
2024-08-09 22:08:29 -0400

    chore: command safety

--- src/main/kotlin/team/ktusers/jam/command/JoinCommand.kt
+++ src/main/kotlin/team/ktusers/jam/command/JoinCommand.kt
@@ -3,7 +3,9 @@ package team.ktusers.jam.command
 import kotlinx.coroutines.Dispatchers
 import net.bladehunt.kotstom.dsl.kommand.buildSyntax
 import net.bladehunt.kotstom.dsl.kommand.kommand
+import net.bladehunt.kotstom.extension.adventure.text
 import net.bladehunt.minigamelib.GameManager
+import net.kyori.adventure.text.format.NamedTextColor.RED
 import team.ktusers.jam.game.JamGame
 
 val JoinCommand = kommand {
@@ -14,6 +16,10 @@ val JoinCommand = kommand {
 
         executorAsync(Dispatchers.Default) {
             val game = GameManager.getOrCreateFirstJoinableGame { JamGame() }
+            if (game.players.contains(player)) {
+                player.sendMessage(text("You are already in this game!", RED))
+                return@executorAsync
+            }
             game.addPlayer(player)
         }
     }
