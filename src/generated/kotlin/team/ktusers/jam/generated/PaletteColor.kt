39e1b713978bc619ef449031f42cb2114ba0f2c0
oglassdev <23040613+oglassdev@users.noreply.github.com>
2024-08-12 10:22:46 -0400

    make PaletteColor serializable (manual)

--- src/generated/kotlin/team/ktusers/jam/generated/PaletteColor.kt
+++ src/generated/kotlin/team/ktusers/jam/generated/PaletteColor.kt
@@ -1,27 +1,28 @@
 // GENERATED!!! DO NOT EDIT
 package team.ktusers.jam.generated
 
-import java.awt.Color
-import kotlin.Int
+import kotlinx.serialization.Serializable
 import net.kyori.adventure.text.format.TextColor
+import java.awt.Color
 
+@Serializable
 public enum class PaletteColor(
-  public val color: Color,
-  public val textColor: TextColor,
+    public val color: Color,
+    public val textColor: TextColor,
 ) {
-  RED(0xff0000),
-  ORANGE(0xff6400),
-  YELLOW(0xffc832),
-  GREEN(0x00ff00),
-  BLUE(0x2828ff),
-  INDIGO(0x4b0082),
-  VIOLET(0x8f00ff),
-  GREY(0x646464),
-  ;
+    RED(0xff0000),
+    ORANGE(0xff6400),
+    YELLOW(0xffc832),
+    GREEN(0x00ff00),
+    BLUE(0x2828ff),
+    INDIGO(0x4b0082),
+    VIOLET(0x8f00ff),
+    GREY(0x646464),
+    ;
 
-  private constructor(colorInt: Int) : this(Color(colorInt), TextColor.color(colorInt))
+    private constructor(colorInt: Int) : this(Color(colorInt), TextColor.color(colorInt))
 
-  public companion object {
-    public fun fromColor(color: Color): PaletteColor? = entries.firstOrNull { it.color == color }
-  }
+    public companion object {
+        public fun fromColor(color: Color): PaletteColor? = entries.firstOrNull { it.color == color }
+    }
 }
